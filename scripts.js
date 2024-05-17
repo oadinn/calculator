2ad6e7da95b046411749da2d31670282411fb3e8
oadinn <duynguyen@Duys-Air.fios-router.home>
2024-05-16 21:56:09 -0400

    calculate function calculates the inputted expression

--- scripts.js
+++ scripts.js
@@ -1,13 +1,65 @@
 const currentOperation = document.getElementById('operation')
 const result = document.getElementById("result")
 
-const calculate = () => {
+const OPERATORS = ['+', '-', '*', '/']
 
+const displayResult = (expression, answer) => {
+    currentOperation.textContent = expression
+    result.textContent = answer
+}
+
+const calculate = (expression) => {
+
+    // find operator
+    let operator = expression.split('')
+    for (let i = 0; i < operator.length; i++) {
+        if (OPERATORS.includes(operator[i])) {
+            operator = operator[i]
+            break;
+        }
+    }
+    
+    // split expression by operator and parse as either float or int
+    let [firstNum, secondNum] = expression.split(operator);
+
+    firstNum.includes('.') ? firstNum = parseFloat(firstNum) : firstNum = parseInt(firstNum)
+    secondNum.includes('.') ? secondNum = parseFloat(secondNum) : secondNum = parseInt(secondNum)
+
+    // calculate expression depending on operator
+    switch (operator) {
+        case '+':
+            displayResult(expression, firstNum + secondNum)
+            break;
+        case '-':
+            displayResult(expression, firstNum - secondNum)
+            break;
+        case '*':
+            displayResult(expression, firstNum * secondNum)
+            break;
+        case '/':
+            displayResult(expression, firstNum / secondNum)
+            break;  
+    }
+}
+
+const alreadyExists = (expression) => {
+    expression = expression.split('')
+    for (let i = 0; i < expression.length; i++) {
+        if (OPERATORS.includes(expression[i])) {
+            console.log(true)
+            return true
+        }
+    }
+    return false;
 }
 
 const displayValue = value => {
     if (value === '.' && result.textContent.includes('.')) {
         return
+    }
+    if (OPERATORS.includes(value) && alreadyExists(result.textContent)) {
+        calculate(result.textContent)
+        return  
     }
     const currentText = result.textContent || ''
     result.textContent = currentText + value
@@ -41,6 +93,8 @@ document.getElementById('plus').addEventListener('click', () => displayValue('+'
 document.getElementById('minus').addEventListener('click', () => displayValue('-'))
 document.getElementById('multiply').addEventListener('click', () => displayValue('*'))
 document.getElementById('divide').addEventListener('click', () => displayValue('/'))
+
+document.getElementById('equal').addEventListener('click', () => calculate(result.textContent))
 
 
 document.getElementById('clear').addEventListener('click', () => clearScreen())
